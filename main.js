c9ec45dfed5cae63114719c5848ff7e6795ecbc2
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-22 12:20:51 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+Crea un array composto da 10 automobili. Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano). Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto. Infine stampa separatamente i 3 array.
+*/
+
+const cars = [
+  { brand: "Ford", model: "Fiesta", fuel: "benzina" },
+  { brand: "Toyota", model: "Corolla", fuel: "diesel" },
+  { brand: "Honda", model: "Civic", fuel: "gpl" },
+  { brand: "Tesla", model: "Model 3", fuel: "elettrico" },
+  { brand: "Fiat", model: "Panda", fuel: "metano" },
+  { brand: "Volkswagen", model: "Golf", fuel: "benzina" },
+  { brand: "BMW", model: "Serie 3", fuel: "diesel" },
+  { brand: "Audi", model: "A4", fuel: "gpl" },
+  { brand: "Mercedes", model: "Classe C", fuel: "elettrico" },
+  { brand: "Peugeot", model: "208", fuel: "metano" },
+];
